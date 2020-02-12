4e67118ac77b248aed49b110bf80a81cb8a90941
RK <shilpeek.auxesis@gmail.com>
2020-02-12 17:26:11 +0530

    commit second2

--- app/src/main/java/com/taxibooking/ui/activity/LoginActivity.java
+++ app/src/main/java/com/taxibooking/ui/activity/LoginActivity.java
@@ -1,19 +1,72 @@
 package com.taxibooking.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
+import com.google.android.material.tabs.TabLayout;
 import com.taxibooking.R;
+import com.taxibooking.ui.login.LoginFragment;
+import com.taxibooking.ui.register.RegisterFragment;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     boolean doubleBackToExitPressedOnce = false;
+    private TabLayout tabLayout;
+    private ViewPager vp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initUI();
+    }
+    private void initUI() {
+        vp =findViewById(R.id.vp);
+        setupViewPager(vp);
+        tabLayout =findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(vp);
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new LoginFragment(), "Login");
+        adapter.addFragment(new RegisterFragment(), "Register");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title.toLowerCase());
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 
     @Override
